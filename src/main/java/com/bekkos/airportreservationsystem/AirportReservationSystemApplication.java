06a4f2d30d726e9de32655072426a5660754792a
bekkos <bekkosm@gmail.com>
2021-03-29 18:01:13 +0200

    Init

---
+++ src/main/java/com/bekkos/airportreservationsystem/AirportReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bekkos.airportreservationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportReservationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirportReservationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bekkos/airportreservationsystem/AirportReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bekkos.airportreservationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportReservationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
